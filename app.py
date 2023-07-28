80fa20d190fdd307db5aba1e65dc7536066b3084
Wilhelm Werwein <werwein.dev@gmail.com>
2023-07-28 14:01:01 +0200

    add exit on user input no

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from rich import print as rprint
 
 # * Functions for Text Styling (rich)
@@ -25,11 +26,22 @@ def get_user_confirmation(prompt):
         else:
             print("Invalid input. Please enter 'yes' or 'no.'")
 
+# * Functions for Functionality
+def print_visible_folders():
+    folders = [item for item in os.listdir(working_path) if os.path.isdir(os.path.join(working_path, item))]
+    visible_folders = [folder for folder in folders if not folder.startswith(".")]
+    
+    if visible_folders:
+        print("Visible folders:")
+        for folder in visible_folders:
+            print(folder)
+    else:
+        print("No visible folders found in the current directory.")
 
 warning = "Read carefully!"
 message = "This Script will remove all .git Folders in All Folders in this directory."
 confirmation = "This cannot be restored. Are you sure about that?"
-workingPath = os.getcwd()
+working_path = os.getcwd()
 
 user_confirmation = "Do you want to proceed?\n"
 
@@ -37,11 +49,13 @@ rprint()
 rprint(make_text_yellow(warning))
 rprint(make_text_bold(message))
 rprint(make_text_red_underlined(confirmation))
-rprint(make_text_cyan_on_dark_gray_background(workingPath))
+rprint(make_text_cyan_on_dark_gray_background(working_path))
 rprint()
 confirmation = get_user_confirmation(user_confirmation)
 if confirmation:
     print("\nThese are all the folders in which .git is getting removed:\n")
 else: 
     print("\nBye.\n")
+    sys.exit()
 
+print_visible_folders()
